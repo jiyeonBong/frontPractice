68014efc4dc25e260ce85e7a4e4d9748b7bd90e4
Jiyeon Bong <bbbccc113@tukorea.ac.kr>
2023-03-08 19:45:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <Navbar bg="success" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">CMT</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#model">모델생성</Nav.Link>
+          <Nav.Link href="#backtesting">백테스팅</Nav.Link>
+          <Nav.Link href="#virtual">모의투자</Nav.Link>
+          <Nav.Link href="#autotrade">실전자동매매</Nav.Link>
+          <Nav.Link href="#coinnews">코인동향</Nav.Link>
+          <Nav.Link href="#notice">공지사항</Nav.Link>
+        </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <a href="#login">로그인</a>
+            {" "}
+            <a href="#signup">회원가입</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
+
+    {/* <div className="Main">
+    메인페이지
+    </div> */}
+    <div className ="loginbox">
+      <div className = "App">
+        <br/>
+        login
+      </div>
+    </div>
+    </>
   );
 }
 
