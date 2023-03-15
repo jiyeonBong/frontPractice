9b2d758b83d08cd33cc3ff7abb3c5720d6966f5b
Jiyeon Bong <bbbccc113@tukorea.ac.kr>
2023-03-16 00:17:40 +0900

    navbar구현

---
+++ src/components/Navibar.js
@@ -0,0 +1,33 @@
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Container, Nav} from 'react-bootstrap'
+
+function Navibar(){
+    return(
+        <div>
+      <Navbar bg="success" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">CMT</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#model">모델생성</Nav.Link>
+          <Nav.Link href="#backtesting">백테스팅</Nav.Link>
+          <Nav.Link href="#virtual">모의투자</Nav.Link>
+          <Nav.Link href="#autotrade">실전자동매매</Nav.Link>
+          <Nav.Link href="#coinnews">코인동향</Nav.Link>
+          <Nav.Link href="#notice">공지사항</Nav.Link>
+        </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <Nav.Link href="#login">로그인</Nav.Link>
+            <Nav.Link href="#signup">회원가입</Nav.Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+    )
+}
+
+
+export default Navibar;
