caabeb8c0a0ad1c84b2e0cf9b6082cf50c486877
Jiyeon Bong <bbbccc113@tukorea.ac.kr>
2023-03-24 00:27:35 +0900

    notice

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -1,6 +1,6 @@
 /*eslint-disable*/
 import React from 'react';
-import { Paper, Box , TextField, FormControl} from '@mui/material';
+import { Button, Paper, Box , TextField, FormControl} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import styled from '@mui/material/styles';
 
@@ -10,9 +10,15 @@ const Notice = () =>{
     return(
      <>
         <Paper variant="outlined" sx={{width: '90%', maxWidth: '1000px', margin: '0 auto', padding: '10px' ,mt:7 }}>
-        <p>공지사항</p>
-        <FormControl></FormControl>
-        <TextField id="standard-search" label="search" type="search" variant="standard" />
+        
+        <div style={{ margin: 20, padding: 20 }}>
+        <Box component="form" noValidate autoComplete="off">
+        공지사항
+        {""}
+        <TextField id="standard-search" label="search" type="search" variant="standard" sx={{ p: "5px" }}/>
+        <Button variant="contained" color="success" sx={{ p: "15px" }}>검색</Button>
+        </Box>
+        </div>
         <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 3 }}>
             
 
