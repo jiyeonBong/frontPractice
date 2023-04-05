81f9d0d51e3dbd3a74ea0aeebcf44477df5255e4
Jiyeon Bong <bbbccc113@tukorea.ac.kr>
2023-04-05 21:32:39 +0900

    autotrading

--- src/components/Pages/AutoTrading.js
+++ src/components/Pages/AutoTrading.js
@@ -1,20 +1,62 @@
 /*eslint-disable*/
-import React from 'react';
-import { TextField, Paper, Typography, Grid,Button,TableCell, TableContainer, Table, TableRow, IconButton } from '@mui/material';
+import React, {useState} from 'react';
+import { Box,TextField, Paper, Typography, Grid,Button,TableCell, TableContainer, Table, TableRow, IconButton } from '@mui/material';
+import DatePicker from '@mui/lab/DatePicker';
 
 const AutoTrading = () => {
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+  
+    const handleStartDateChange = (date) => {
+      setStartDate(date);
+    };
+  
+    const handleEndDateChange = (date) => {
+      setEndDate(date);
+    };
+  
+    const handleApplyButtonClick = () => {
+      console.log('Selected date range:', startDate, endDate);
+    };
+  
+    const handleClearButtonClick = () => {
+      setStartDate(null);
+      setEndDate(null);
+    };
     return(
         <>
         <Grid item xs={12} sm={7.5}>
         <Paper sx={{ padding:10, display: 'flex', justifyContent: 'center' }}>
         <Grid container justifyContent="space-between" alignItems="center" sx={{ marginBottom: 8 }}>
-        <Grid item>
+            <Box display="flex" alignItems="center">
+            <DatePicker
+                label="Start Date"
+                value={startDate}
+                onChange={handleStartDateChange}
+                renderInput={(params) => <TextField {...params} />}
+                sx={{ mr: 1 }}
+            />
+            <DatePicker
+                label="End Date"
+                value={endDate}
+                onChange={handleEndDateChange}
+                renderInput={(params) => <TextField {...params} />}
+                sx={{ mr: 1 }}
+            />
+            <Box flexGrow={1} />
+            <Button onClick={handleApplyButtonClick}>Apply</Button>
+            <Button onClick={handleClearButtonClick}>Clear</Button>
+            </Box>
+        {/* <Grid item>
             기간선택: 
-            에서  
+            <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+            에서 
+            <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
+            까지 
             모델선택
-            <Button>매매 시작</Button>
-            <Button>매매 중지</Button>
-        </Grid>
+            <Button variant="contained" color="success">매매 시작</Button>{" "}
+            <Button variant="contained" color="success">매매 중지</Button>
+        </Grid> */}
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableRow>
